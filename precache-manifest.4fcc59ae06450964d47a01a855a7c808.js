5c33e81892fe008db1530fce784bcf000aea6c5a
Ahmad Hadi Jaelani <ahmadhadijaelani@gmail.com>
2020-02-13 14:10:18 +0700

    Updates

---
+++ precache-manifest.4fcc59ae06450964d47a01a855a7c808.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "321e19bcc6d2580818d464a61fe0f34b",
+    "url": "/ajhadi/index.html"
+  },
+  {
+    "revision": "d66e3226ca0a040a811e",
+    "url": "/ajhadi/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6db313057c23e2618477",
+    "url": "/ajhadi/static/js/2.9fffd79b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ajhadi/static/js/2.9fffd79b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d66e3226ca0a040a811e",
+    "url": "/ajhadi/static/js/main.8c7d942e.chunk.js"
+  },
+  {
+    "revision": "527a640fd7809991ef94",
+    "url": "/ajhadi/static/js/runtime-main.cf4d02de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ajhadi/static/media/logo.5d5d9eef.svg"
+  }
+]);
